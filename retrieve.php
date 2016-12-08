d634dc676ea579ffeaadafce9603b76fe5a42c9c
Sonya Roy <sonyaroy75@gmail.com>
2016-12-08 20:30:16 +0530

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(0);
 $nam = $_GET["id"];
-$content=file_get_contents("playerdata/".$nam.".json");
-echo $content;
+$fp=fopen("playerdata/".$nam.".json",'r');
+$dat=fread($fp,filesize("playerdata/".$nam.".json"));
+header("Content-Encoding:gzip");
+echo $dat;
 ?>

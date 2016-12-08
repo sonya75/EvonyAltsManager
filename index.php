a0abb982317c7d8c8a2cd45fc3a925bea63ebde3
Sonya Roy <sonyaroy75@gmail.com>
2016-12-08 23:59:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,9 @@ $resources=$_POST['summary_resources'];
 $troops=$_POST["summary_troops"];
 $attacks=$_POST["summary_attacks"];
 $username=$_POST["summary_username"];
-$query="CREATE TABLE IF NOT EXISTS ACCOUNTS_SUMMARY ( RESOURCES TEXT, TROOPS TEXT, ATTACKS INT, LASTUPDATED INT, NAME TINYTEXT, ID TINYTEXT);";
+$query="CREATE TABLE IF NOT EXISTS ACCOUNTS_SUMMARY ( RESOURCES TEXT, ALLIANCE TINYTEXT, TROOPS TEXT, ATTACKS INT, LASTUPDATED INT, NAME TINYTEXT, ID TINYTEXT);";
+$conn->query($query);
+$query="ALTER IGNORE TABLE ACCOUNTS_SUMMARY ADD ALLIANCE TINYTEXT;";
 $conn->query($query);
 $check=$conn->prepare("SELECT COUNT(*) FROM ACCOUNTS_SUMMARY WHERE ID= ? ");
 $check->bind_param('s',$id);
@@ -36,35 +38,33 @@ $check->execute();
 $check->bind_result($rows);
 $check->fetch();
 $check->close();
-if ($rows>0){
-	$stm=$conn->prepare("UPDATE ACCOUNTS_SUMMARY SET NAME= ? , RESOURCES= ? , TROOPS= ? , ATTACKS= ? , LASTUPDATED = ? WHERE ID= ?");
-	$curtime=time();
-	$stm->bind_param('sssdds',$username,$resources,$troops,$attacks,$curtime,$id);
-	$stm->execute();
-	$stm->close();
-}
-else{
-	$stm=$conn->prepare("INSERT INTO ACCOUNTS_SUMMARY (NAME, RESOURCES, TROOPS, ATTACKS, ID, LASTUPDATED ) VALUES ( ? , ? , ? , ? , ? , ? )");
-	$curtime=time();
-	$stm->bind_param('sssdsd',$username,$resources,$troops,$attacks,$id,$curtime);
-	$stm->execute();
-	$stm->close();
-}
-$conn->close();
 $data=$_POST['player'];
-$fp=fopen("efef",'w');
-fwrite($fp,$data);
-fclose($fp);
 $rawdata=base64_decode($data);
 $data=gzuncompress($rawdata);
-$fp=gzopen("playerdata/".$id.".json",'w');
 $amf = new AMFObject($data);
 $deserializer = new AMFDeserializer($amf->rawData);
 $json=($deserializer->readAmf3Data());
 $js= array();
 $js['player']=$json;
 $js['server']=$json['server'];
-$js['warlog']=$json['warlog'];
+$js['warlog']=$_POST['warlog'];
+$alliance=$json['playerInfo']['alliance'];
+if ($rows>0){
+	$stm=$conn->prepare("UPDATE ACCOUNTS_SUMMARY SET NAME= ? , ALLIANCE= ?, RESOURCES= ? , TROOPS= ? , ATTACKS= ? , LASTUPDATED = ? WHERE ID= ?");
+	$curtime=time();
+	$stm->bind_param('ssssdds',$username,$alliance,$resources,$troops,$attacks,$curtime,$id);
+	$stm->execute();
+	$stm->close();
+}
+else{
+	$stm=$conn->prepare("INSERT INTO ACCOUNTS_SUMMARY (NAME, ALLIANCE, RESOURCES, TROOPS, ATTACKS, ID, LASTUPDATED ) VALUES ( ? , ? , ? , ? , ? , ? )");
+	$curtime=time();
+	$stm->bind_param('ssssdsd',$username,$alliance,$resources,$troops,$attacks,$id,$curtime);
+	$stm->execute();
+	$stm->close();
+}
+$conn->close();
+$fp=gzopen("playerdata/".$id.".json",'w');
 gzwrite($fp,json_encode($js));
 gzclose($fp);
 ?>

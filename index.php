185ba3ad696ad6bbe15d2a6401bdd5f021d69e09
Sonya Roy <sonyaroy75@gmail.com>
2016-12-14 03:15:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+ob_start();
 error_reporting(0);
-require_once( 'amfphp/core/amf/app/Gateway.php');
+require_once( 'Amfphp/Core/amf/app/Gateway.php');
 require_once( AMFPHP_BASE . 'amf/io/AMFSerializer.php');
 require_once( AMFPHP_BASE . 'amf/io/AMFDeserializer.php');
 echo "LOADED";
